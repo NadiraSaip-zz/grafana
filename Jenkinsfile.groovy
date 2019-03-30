c9c116885907abd49fa1103078f848b4b2683cad
Farkhod <sadykovfarkhod@gmail.com>
2019-03-30 11:53:05 -0500

    Added grafana

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,14 +10,14 @@ node {
   stage('Build docker image') {
 
       // Build the docker image
-      app = docker.build("fsadykov/grafana", "-f ${WORKSPACE}/Dockerfile .")
+      app = docker.build("fuchicorp/grafana", "-f ${WORKSPACE}/Dockerfile .")
   }
 
 
   stage('Push image') {
 
      // Push docker image to the Docker hub
-      docker.withRegistry('', 'dockerhub-credentials') {
+      docker.withRegistry('', 'fuchicorp-dockerhub') {
           app.push("0.${BUILD_NUMBER}")
           app.push("latest")
       }
